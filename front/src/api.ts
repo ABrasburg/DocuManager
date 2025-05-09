60f23217aee26bd6e2fa8a0307c14a02b28751a6
ABrasburg <abrasburg@fi.uba.ar>
2025-05-09 09:14:26 -0300

    modifico donde esta el back

--- front/src/api.ts
+++ front/src/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: `http://${window.location.hostname}:9000`,
 });
 
 export default api;
