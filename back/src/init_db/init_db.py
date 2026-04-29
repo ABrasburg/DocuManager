044caf7c1f70c97b6ffc4d276042ef84526ec355
ABrasburg <abrasburg@fi.uba.ar>
2026-04-29 19:09:15 -0300

    Fix: deshabilitar init_db incompatible con modelo multi-farmacia

--- back/src/init_db/init_db.py
+++ back/src/init_db/init_db.py
@@ -1,31 +1,5 @@
 from sqlalchemy.orm import Session
-from src.models.tipo_comprobante import TipoComprobante
-from src.models.id_zeta import IdZeta
 
 
 def init_db(db: Session):
-    tipos_comprobantes = [(1, "Factura"), (2, "Nota de Débito"), (3, "Nota de Crédito")]
-    id_zetas  = [(8, 0), (9, 0)]
-
-    # Crear los tipos de comprobantes
-    for tipo_comprobante in tipos_comprobantes:
-        existing_tipo_comprobante = (
-            db.query(TipoComprobante)
-            .filter_by(tipo_comprobante=tipo_comprobante[0])
-            .first()
-        )
-        if not existing_tipo_comprobante:
-            tipo_comprobante_obj = TipoComprobante(
-                tipo_comprobante=tipo_comprobante[0], nombre=tipo_comprobante[1]
-            )
-            db.add(tipo_comprobante_obj)
-    
-    # Crear los id_zeta
-    for id_zeta in id_zetas:
-        existing_id_zeta = db.query(IdZeta).filter_by(id=id_zeta[0]).first()
-        if not existing_id_zeta:
-            id_zeta_obj = IdZeta(id=id_zeta[0], contador=id_zeta[1])
-            db.add(id_zeta_obj)
-
-    # Confirmar todos los cambios
-    db.commit()
+    pass
